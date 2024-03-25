3ed641c3db19fe25c0ff46df6d5f8d1b6367370f
ArkadiKorotitshTARpv22 <gentletin62@gmail.com>
2024-03-25 16:30:33 +0200

    [#187304684] - uuendatud skriptid

--- tarkvarascript.js
+++ tarkvarascript.js
@@ -1,104 +1,106 @@
-function randomSyna(){
-    //esimese masiivi loomine
-    //sonad
-    const synad=[
-        'teostus',
-        'algoritm',
-        'kavand',
-        'kasutajaliides',
-        'mudel',
-        'andmebaasi skeem',
-        'standart',
-        'tsükkel',
-        'andmetöötlus',
-        'andmestruktuur',
-        'arenduskeskkond',
-        'projektihaldus tööriist',
-        'kavandamine',
-        'iteratsioon',
-        'parandus',
-        'parandamine',
-        //koskmudel,agiilne mudel,spiraalne mudel,inkrementaalne mudel,nõudmised,realiseerimine,testimine,integreerimine,kasutamine,hooldus,eelised,puudused,elutsükkel,arendamine,valideerimine
-    ];
-    //random synad
-    const juhuslikSyna=Math.floor(Math.random()*synad.length);
+const synad = [
+    'teostus',
+    'algorütm',
+    'kavand',
+    'kasutajaliides',
+    'mudel',
+    'andmebaasi skeem',
+    'standart',
+    'tsükkel',
+    'andmetöötlus',
+    'andmestruktuur',
+    'arenduskeskkond',
+    'projektihaldus tööriist',
+    'kavandamine',
+    'iteratsioon',
+    'parandus',
+    'koskmudel',
+    'agiilne mudel',
+    'spiiralne mudul',
+    'inkrementaalne mudel',
+    'nõudmised',
+    'reliseerimine',
+    'testimine',
+    'integreerimine',
+    'kasutamine',
+    'eelised',
+    'puudused',
+    'elutsükkel',
+    'arendamine',
+    'validerimine'
+];
 
-    //võtame random sõnad massivist
-    const syna=synad[juhuslikSyna];
-    //lisame html-lehele
-    document.getElementById("random-syna").innerHTML=syna;
+
+const venesynad = [
+    'реализация',
+    'алгоритм',
+    'план',
+    'пользовательский интерфейс',
+    'модель',
+    'схема базы данных',
+    'стандарт',
+    'цикл',
+    'обработка данных',
+    'структура данных',
+    'среда разработки',
+    'инструмент управления проектами',
+    'планирование',
+    'итерация',
+    'исправление',
+    'каскмодель',
+    'гибкая модель',
+    'пространственная модель',
+    'Инкрементальная модель',
+    'требования',
+    'переселение',
+    'тестирование',
+    'интеграция',
+    'использование',
+    'выгоды',
+    'недостатки',
+    'Циклжизни',
+    'разработка',
+    'подтверждение'
+];
+
+
+function randomSyna() {
+    const juhuslikSyna = Math.floor(Math.random() * synad.length);
+    const syna = synad[juhuslikSyna];
+    document.getElementById("random-sona").innerHTML = syna;
 }
 
-function answerSyna(){
-    let syna=document.getElementById("random-syna")
-    let answerbox=document.getElementById("kontroll")
-    let result=document.getElementById("vastus")
-    if (syna.innerHTML==='teostus' && answerbox.value.toLowerCase()==='реализация'){
-        result.innerHTML='õige'
-        result.style.color="green"
+function randomSynaRU() {
+    const juhuslikSona = Math.floor(Math.random() * venesynad.length);
+    const syna = venesynad[juhuslikSona];
+    document.getElementById("random-vene-sona").innerHTML = syna;
+}
+
+
+function answerSyna() {
+    const syna = document.getElementById("random-sona").innerText;
+    const venesyna = document.getElementById("kontroll").value.toLowerCase();
+    const indEesti = synad.indexOf(syna);
+    const indVene = venesynad.indexOf(venesyna);
+    if (indEesti === indVene && indEesti !== -1 && indVene !== -1) {
+        document.getElementById("vastus2").innerHTML = "Õige";
+        document.getElementById("vastus2").style.color = "green"
+    } else {
+        document.getElementById("vastus2").innerHTML = "Vale";
+        document.getElementById("vastus2").style.color = "red"
     }
-    else if (syna.innerHTML==='algoritm' && answerbox.value.toLowerCase()==='алгоритм'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='kavand' && answerbox.value.toLowerCase()==='проект'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='kasutajaliides' && answerbox.value.toLowerCase()==='интерфейс пользователя'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='mudel' && answerbox.value.toLowerCase()==='модель'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='andmebaasi skeem' && answerbox.value.toLowerCase()==='схема базы данных'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='standart' && answerbox.value.toLowerCase()==='стандарт'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='tsükkel' && answerbox.value.toLowerCase()==='цикл'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='andmetöötlus' && answerbox.value.toLowerCase()==='обработка данных'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='andmestruktuur' && answerbox.value.toLowerCase()==='структура данных'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='arenduskeskkond' && answerbox.value.toLowerCase()==='среда разработки'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='projektihaldus tööriist' && answerbox.value.toLowerCase()==='инструмент управления проектами'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='kavandamine' && answerbox.value.toLowerCase()==='планирование'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='iteratsioon' && answerbox.value.toLowerCase()==='итерация'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='parandus' && answerbox.value.toLowerCase()==='коррекция'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else if (syna.innerHTML==='parandamine' && answerbox.value.toLowerCase()==='коррекция'){
-        result.innerHTML='õige'
-        result.style.color="green"
-    }
-    else {
-        result.innerHTML='vale'
-        result.style.color="red"
+}
+
+function answerSynaRU() {
+    const syna = document.getElementById("random-vene-sona").innerText;
+    const eestisona = document.getElementById("kontrollru").value.toLowerCase();
+    const indEesti = synad.indexOf(eestisona);
+    const indVene = venesynad.indexOf(syna);
+    if (indEesti === indVene && indEesti !== -1 && indVene !== -1) {
+        document.getElementById("vastusru").innerHTML = "Õige";
+        document.getElementById("vastusru").style.color = "green"
+    } else {
+        document.getElementById("vastusru").innerHTML = "Vale";
+        document.getElementById("vastusru").style.color = "red"
     }
 }
